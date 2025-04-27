d2fccaa8cd9b3b48ffc2ce13ff0a43a60a01213d
bithi25 <bithibhowmik246@gmail.com>
2025-04-27 10:35:34 +0600

    annimation done

--- java.js
+++ java.js
@@ -3,3 +3,6 @@ const header = document.querySelector("header")
 window.addEventListener ("scroll",function(){
     header.classList.toggle("sticky", this.window.scrollY > 0);
 })
+AOS.init({
+    duration: 1000, // values from 0 to 3000, with step 50ms
+});
